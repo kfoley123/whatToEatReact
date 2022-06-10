1f6a5181a1cb8699ee99797a9edc91846465ecb0
Kortney <kortney@iMac.local>
2022-06-10 15:45:54 -0300

    made component for Recipe and connected it to the main js file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import Recipe from "./components/Recipe/Recipe";
 
 function App() {
     return (
         <div>
             <Header />
+            <Recipe />
         </div>
     );
 }
