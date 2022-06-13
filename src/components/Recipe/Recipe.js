b61ecc2f5f5cfa485d0f2d15de0fc4f8c435fb1a
Kortney <kortney@iMac.local>
2022-06-13 11:07:33 -0300

    used useState to set recipe variable and used useEffect to call the API. Looped through each ingredient and the amounts and pushed them into a variable combostring. Pushed the combostring variable to the ingrdients array. Displayed ingredients array, photo, food type, instructions to the screen

--- src/components/Recipe/Recipe.js
+++ src/components/Recipe/Recipe.js
@@ -1,12 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-export default function Recipe(props) {
+export default function Recipe() {
+    const [recipe, setRecipe] = useState({});
+    let ingredients = [];
+
+    useEffect(() => {
+        fetch("https://www.themealdb.com/api/json/v1/1/random.php")
+            .then((response) => response.json())
+            .then((response) => {
+                setRecipe(response.meals[0]);
+            });
+    }, []);
+
+    for (let i = 1; i < 20; i++) {
+        let comboString =
+            recipe[`strIngredient${i}`] + " - " + recipe[`strMeasure${i}`];
+        ingredients.push(comboString);
+    }
+
     return (
-        <div className="recipeContainer ">
-            <h1>recipe will go here</h1>
-            {props.ingredients.map((ingredient) => {
-                <li>{ingredient}</li>;
-            })}
+        <div className="recipeContainer">
+            <img src={recipe.strMealThumb}></img>
+            <h1>{recipe.strMeal}</h1>
+            <h2>Type: {recipe.strArea}</h2>
+            <h3> {recipe.strCategory} </h3>
+            <p> {recipe.strInstructions} </p>
+            <ul>
+                {ingredients.map((ingredient) => (
+                    <li>{ingredient}</li>
+                ))}
+            </ul>
         </div>
     );
 }
