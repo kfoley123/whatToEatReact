b36cfa956bb8695ac6bb7288d2ca2ddc8ccc77a6
Kortney <kortney@iMac.local>
2022-08-29 11:34:13 -0300

    fixed error where null was being listed as ingredient, and where there was still a - when there was no measurement

--- src/components/RecipeCard/RecipeCard.js
+++ src/components/RecipeCard/RecipeCard.js
@@ -4,16 +4,19 @@ export default function RecipeCard(props) {
     let ingredients = [];
 
     for (let i = 1; i < 20; i++) {
-        let comboString =
-            props.recipe[`strIngredient${i}`] +
-            " - " +
-            props.recipe[`strMeasure${i}`];
-        ingredients.push(comboString);
+        if (props.recipe[`strIngredient${i}`] !== null) {
+            let comboString = props.recipe[`strIngredient${i}`];
+
+            if (props.recipe[`strMeasure${i}`] !== " ") {
+                comboString += " - " + props.recipe[`strMeasure${i}`];
+            }
+            ingredients.push(comboString);
+        }
     }
 
     if (Object.keys(props.recipe).length !== 0) {
         return (
-            <div className="py-30 flex justify-center mb-10 ">
+            <div className="py-30 flex justify-center mb-10">
                 <div className="max-w-xl rounded overflow-hidden shadow-lg">
                     <img
                         className="w-full"
@@ -25,7 +28,7 @@ export default function RecipeCard(props) {
                     </h1>
                     <div className="p-6">
                         <h3 className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-5">
-                            Category: {props.recipe.strCategory}{" "}
+                            Category: {props.recipe.strCategory}
                         </h3>
                         <h2 className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">
                             Type: {props.recipe.strArea}
